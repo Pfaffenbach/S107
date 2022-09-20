6adb5b80d12f5829e6a4010acbd17241f1256f0a
Erich <erich.silveira@ges.inatel.br>
2022-09-19 21:27:44 -0300

    ADD exercicio1

---
+++ exercicio1/src/main/java/CupomDesconto.java
@@ -0,0 +1,16 @@
+public class CupomDesconto {
+
+    private double desconto;
+
+    public CupomDesconto(double desconto) {
+        this.desconto = desconto;
+    }
+
+    public double getDesconto() {
+        return desconto;
+    }
+
+    public void setDesconto(double desconto) {
+        this.desconto = desconto;
+    }
+}
